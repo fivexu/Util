0021135614387d7a822576106b4d698d75e9963a
fivexu <316643470@qq.com>
2019-12-11 18:02:09 +0800

    #fix  优化 图片压缩

--- vueConfig/vue.config.js
+++ vueConfig/vue.config.js
@@ -4,24 +4,42 @@ const __resolve = dir => {
   return path.join(__dirname, dir);
 };
 
+// 压缩图片
+const minImage = config => {
+  const imagesRule = config.module.rule("images");
+  imagesRule
+    .use("image-webpack-loader")
+    .loader("image-webpack-loader")
+    .options({
+      bypassOnDebug: true
+    })
+    .end();
+};
+
+// 重定向路径
+const redirect = config => {
+  config.resolve.alias
+    .set("api", __resolve("src/api"))
+    .set("base", __resolve("src/base"))
+    .set("utils", __resolve("src/utils"))
+    .set("mixin", __resolve("src/mixin"))
+    .set("assets", __resolve("src/assets"))
+    .set("config", __resolve("src/config"))
+    .set("common", __resolve("src/common"))
+    .set("components", __resolve("src/components"));
+};
+
 module.exports = {
   publicPath: "./",
   outputDir: "dist",
   lintOnSave: false,
   chainWebpack: config => {
-    config.resolve.alias
-      .set("api", __resolve("src/api"))
-      .set("base", __resolve("src/base"))
-      .set("utils", __resolve("src/utils"))
-      .set("mixin", __resolve("src/mixin"))
-      .set("assets", __resolve("src/assets"))
-      .set("config", __resolve("src/config"))
-      .set("common", __resolve("src/common"))
-      .set("components", __resolve("src/components"));
+    redirect(config);
+    minImage(config);
   },
   devServer: {
     open: true,
-    port: 8598,
+    port: 8040,
     https: false,
     hotOnly: false
   }
